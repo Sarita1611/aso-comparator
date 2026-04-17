83d8013840d2ca1d13376d05d55f4e55531caea4
Sarita <saritayadav373118@gmail.com>
2026-04-17 13:45:15 +0530

    Update logo image width in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center group">
-              <img src={logo} alt="Logo" className="w-auto max-w-[260px]" />
+              <img src={logo} alt="Logo" className="w-auto max-w-[141px]" />
           </Link>
 
           {/* Nav links */}
